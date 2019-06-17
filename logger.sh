63778a0f96e081e47a7b16183ee7fef2b0d019fb
root <root@master-1.ocp311d.lab.rdu2.cee.redhat.com>
2019-06-17 16:52:25 -0400

    Updated logger.sh to include sequence number

--- logger.sh
+++ logger.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+SEQ=0
 while true; do
 
-echo "$(date) Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more."
+if [[ $SEQ = 1000000 ]];then
+SEQ=0
+fi
+
+echo "$(date) | $SEQ | $OPENSHIFT_BUILD_NAMESPACE/$HOSTNAME | Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more. Some logs, More Logs, even more, and more."
+
+SEQ=$(($SEQ+1))
+
+sleep 1
 
 done
